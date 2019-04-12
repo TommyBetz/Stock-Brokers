5b2afd754bcb25cb4a61ae88f0276056e51cfc10
helvetien <helvetien@gmail.com>
2019-04-12 15:45:53 -0700

    added comments

--- Financial_News_Sentiment.py
+++ Financial_News_Sentiment.py
@@ -1,4 +1,4 @@
-# Financial times dataset 01-2018 to 05-2018from pyspark.context import SparkContext
+# Financial times dataset 01-2018 to 05-2018
 import sys
 assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 from pyspark.sql.session import SparkSession
@@ -10,14 +10,12 @@ from pyspark.sql import SparkSession, functions, types
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
 
-
-# sc = SparkContext('local')
-# spark = SparkSession(sc)
-
 sc =SparkContext.getOrCreate()
 spark = SparkSession(sc)
 
-lst_companies = ['apple','facebook']
+###################################################
+
+lst_companies = ['apple','facebook'] # companies to analyse
 
 def organizationsAndsentiment(x):
 	entities = []
@@ -26,6 +24,7 @@ def organizationsAndsentiment(x):
 		entities += entity
 	return entities
 
+# Convert the pre existing textual sentiment to a numerical value
 def sentiment(x):
 	str_company = x[0]
 	lst = x[1].split(',')
@@ -47,8 +46,8 @@ def main(inputs,output):
 	
 	count = 0
 
+	# do this loop for each company to analyze
 	for company in lst_companies:
-		#print(company)
 		count = count + 1
 		# READ DATA
 		df_temp = spark.read.json('file:///home/vdhununj/BigData2/Project/Financial_News/'+inputs)
@@ -84,9 +83,6 @@ def main(inputs,output):
 			df_final = df_temp
 		else:
 			df_final = df_final.union(df_temp)
-			
-		
-		#df_final.show()
 		
 	# CALCULATE MEDIAN
 
@@ -94,15 +90,11 @@ def main(inputs,output):
 	df_median = spark.sql("SELECT date, company, percentile_approx(sentiment, 0.5) as median_sentiment from medianTbl group by date, company ")
 	spark.catalog.dropTempView("medianTbl")
 		
-	# df_summary.select('median_sentiment_apple').show()
-	#df_median.show()
 
 	###### AGGREGATIONS ###################################################################
 
 	## WEIGHTED SENTIMENTS
 	df_sentiment = df_final.withColumn('weighted_sentiment', df_final.shares_likes * df_final.sentiment)
-	# df_sentiment.select(df_final.shares_likes, df_final.sentiment, 'weighted_sentiment').show()
-	#df_sentiment.show()
 
 	#### CALCULATE AGGREGATIONS
 
@@ -116,17 +108,13 @@ def main(inputs,output):
 			functions.avg(df_summary.weighted_sentiment).alias('avg_weighted_sentiment_1') # the average of the overall weighted average sentiment values
 			)
 
-	#df_summary.show()
 
 	### JOIN MEDIAN
 
 	df_summary = df_summary.join(df_median, ['date', 'company'])
-	#df_summary.show()
 
 	df_summary2 = df_summary.withColumn('avg_weighted_sentiment_2', \
 		df_summary.sum_weighted_sentiment / df_summary.count_articles)
-
-	#df_summary2.show()
 
 	df_summary2 = df_summary2.join(df_sentiment, [df_summary.date == df_sentiment.date, \
 		df_summary.company == df_sentiment.company, df_sentiment.shares_likes == df_summary.max_shares_likes]) \
@@ -135,7 +123,6 @@ def main(inputs,output):
 		df_summary.sum_weighted_sentiment, df_summary.avg_weighted_sentiment_1, df_summary2.avg_weighted_sentiment_2,\
 		df_sentiment.title, df_sentiment.text)
 
-	#df_summary2.show()
 	df_summary2 = df_summary2.dropDuplicates(['date','company'])
 
 	df_summary3 = df_summary2.toPandas()
